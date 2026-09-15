c09e3e582e4e37488d4a893f72bdda88e33faa2a
Gaël Close <closega@gmail.com>
2026-09-15 23:03:04 +0200

    Fix supplementary path and ignore marimo

--- paper/src/my_package/config.py
+++ paper/src/my_package/config.py
@@ -9,7 +9,7 @@ PROJ_ROOT = here()
 logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
 DATA_DIR = PROJ_ROOT / "data"
-DIST_DIR = PROJ_ROOT / "dist"
+SUPP_DIR = PROJ_ROOT / "dist" / "supplementary"
 
 # Venv
 logger.info(f"Python executable: {sys.executable}")
